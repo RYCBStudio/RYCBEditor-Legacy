21b5d02382e56fb88faeb63531dac82a2222641c
QYF-RYCBStudio <74858895+QYF-RYCBStudio@users.noreply.github.com>
2023-01-26 19:10:26 +0800

    Add files via upload

---
+++ IDE/RYCBCodeSense.cs
@@ -0,0 +1,45 @@
+﻿using ICSharpCode.AvalonEdit.CodeCompletion;
+using ICSharpCode.AvalonEdit.Document;
+using ICSharpCode.AvalonEdit.Editing;
+using IDE.Properties;
+using System;
+using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace IDE
+{
+    internal class RYCBCodeSense : ICompletionData
+    {
+        private CodeSenseType _type;
+
+        public RYCBCodeSense(string text, CodeSenseType type, string desc)
+        {
+            this.Text = text;
+            this._type = type;
+            this.Description = desc;
+        }
+
+        public ImageSource Image => _type switch
+        {
+            CodeSenseType.FIELD => Imaging.CreateBitmapSourceFromHBitmap(Resources.field.GetHbitmap(), IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions()),
+            CodeSenseType.FUNC => Imaging.CreateBitmapSourceFromHBitmap(Resources.func.GetHbitmap(), IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions()),
+            CodeSenseType.KEYWORD => Imaging.CreateBitmapSourceFromHBitmap(Resources.keyword.GetHbitmap(), IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions()),
+            _ => Imaging.CreateBitmapSourceFromHBitmap(Resources.help.GetHbitmap(), IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions()),
+        };
+
+        public string Text { get; set; }
+
+        public object Content => this.Text;
+
+        public object Description { get; set; }
+
+        public double Priority { get; }
+
+        public void Complete(TextArea textArea, ISegment completionSegment, EventArgs insertionRequestEventArgs)
+        {
+            textArea.Document.Replace(completionSegment, Text);
+        }
+    }
+}
---
+++ IDE/Terminal.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace IDE
+{
+    partial class Terminal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Terminal));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.text = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F);
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.button1.Image = global::IDE.Properties.Resources.delete;
+            this.button1.Location = new System.Drawing.Point(869, -1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(40, 36);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Close);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold);
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.button2.Location = new System.Drawing.Point(836, -5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(46, 59);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "___";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.Min);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 33);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(885, 489);
+            this.panel1.TabIndex = 2;
+            // 
+            // text
+            // 
+            this.text.AutoSize = true;
+            this.text.Location = new System.Drawing.Point(43, 7);
+            this.text.Name = "text";
+            this.text.Size = new System.Drawing.Size(39, 20);
+            this.text.TabIndex = 3;
+            this.text.Text = "终端";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::IDE.Properties.Resources.detail;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(25, 25);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Terminal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(909, 534);
+            this.ControlBox = false;
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.text);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F);
+            this.ForeColor = System.Drawing.SystemColors.Control;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.IsMdiContainer = true;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Terminal";
+            this.Text = "Terminal";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        public System.Windows.Forms.Label text;
+    }
+}
