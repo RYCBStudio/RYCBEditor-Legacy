a440ff1a746bd922a43f27ce849772661283b82e
QYF-RYCBStudio <qyf090506@163.com>
2024-04-13 22:22:59 +0800

    .

--- Help/About.Designer.cs
+++ Help/About.Designer.cs
@@ -34,14 +34,17 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 17F, System.Drawing.FontStyle.Bold);
-            this.label1.Location = new System.Drawing.Point(137, 38);
+            this.label1.Location = new System.Drawing.Point(157, 36);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(193, 39);
             this.label1.TabIndex = 0;
@@ -51,7 +54,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe Print", 13F, System.Drawing.FontStyle.Underline);
-            this.label2.Location = new System.Drawing.Point(137, 76);
+            this.label2.Location = new System.Drawing.Point(157, 74);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(280, 38);
             this.label2.TabIndex = 3;
@@ -71,7 +74,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft YaHei UI", 12.5F);
-            this.label3.Location = new System.Drawing.Point(139, 114);
+            this.label3.Location = new System.Drawing.Point(159, 112);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(140, 28);
             this.label3.TabIndex = 5;
@@ -81,7 +84,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F);
-            this.label4.Location = new System.Drawing.Point(140, 142);
+            this.label4.Location = new System.Drawing.Point(160, 168);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(383, 20);
             this.label4.TabIndex = 7;
@@ -92,15 +95,41 @@
             this.pictureBox1.Image = global::IDE.Properties.Resources.IDE_ico_1024x_;
             this.pictureBox1.Location = new System.Drawing.Point(3, 38);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(128, 128);
+            this.pictureBox1.Size = new System.Drawing.Size(150, 150);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 6;
             this.pictureBox1.TabStop = false;
             // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = global::IDE.Properties.Resources.GitHubLogo_Light;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox2.Location = new System.Drawing.Point(159, 136);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(32, 32);
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Microsoft YaHei UI Light", 12F);
+            this.linkLabel1.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.linkLabel1.LinkArea = new System.Windows.Forms.LinkArea(0, 11);
+            this.linkLabel1.Location = new System.Drawing.Point(197, 140);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(280, 32);
+            this.linkLabel1.TabIndex = 9;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Open-source (GPL-3 License)";
+            this.linkLabel1.UseCompatibleTextRendering = true;
+            // 
             // About
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
-            this.ClientSize = new System.Drawing.Size(571, 171);
+            this.ClientSize = new System.Drawing.Size(689, 193);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label3);
@@ -121,6 +150,7 @@
             this.ZoomScaleRect = new System.Drawing.Rectangle(19, 19, 443, 156);
             this.Load += new System.EventHandler(this.Form2_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -134,5 +164,7 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
     }
 }
