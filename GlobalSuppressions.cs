413d26d3c943d81ecc307edbc0178259df792435
QYF-RYCBStudio <qyf090506@163.com>
2024-04-20 11:09:53 +0800

    Sync to Dev.

--- GlobalSuppressions.cs
+++
@@ -1,9 +0,0 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given
-// a specific target and scoped to a namespace, type, member, etc.
-using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage("Style", "IDE0161:转换为文件范围限定的 namespace", Justification = "<挂起>", Scope = "namespace", Target = "~N:IDE")]
-[assembly: SuppressMessage("Style", "IDE0044:添加只读修饰符", Justification = "<挂起>", Scope = "namespace", Target = "~N:IDE")]
-[assembly: SuppressMessage("Usage", "VSTHRD100:Avoid async void methods", Justification = "<挂起>", Scope = "namespace", Target = "~N:IDE")]
